ba00c589c5206c59f9a648015297ce316d8c0cb6
haram8009 <haram8009@gmail.com>
2022-09-30 02:14:15 +0900

    password 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -28,6 +30,24 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number 
+    }
+})
+
+userSchema.pre('save', function( next ) {
+    var user = this;
+    // 조건 : 이메일이나 다른것이 아닌 비밀번호를 다시 저장할때 만 암호화 새로 하도록!
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            
+            bcrypt.hash(user.password, salt, function(err, hash) { // hash가 암호화된 비밀번호
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next() // 이게 없으면 여기 계속 머물게됨
     }
 })
 
