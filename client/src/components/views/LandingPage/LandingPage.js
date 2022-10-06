073c925c8da1f23fcc0853bbdd592cedfe566d13
haram8009 <haram8009@gmail.com>
2022-10-07 00:59:56 +0900

    로그아웃 기능

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,26 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
-  // 랜딩 페이지에 들어오자마자 이걸 함
+  const navigate = useNavigate();
+
   useEffect(()=> {
     axios.get('/api/hello')
     .then(response => console.log(response.data))
-  }, []) // 서버로 보냄
+  }, [])
+
+  const onClickHandler = () => {
+    axios.get('/api/users/logout')
+      .then(response => {
+        // console.log(response.data)
+        if(response.data.success) {
+          navigate('/login')
+        } else {
+          alert('로그아웃 하는데 실패 했습니다.')
+        }
+      })
+  }
 
   return (
     <div style={{
@@ -14,6 +28,10 @@ function LandingPage() {
       , width: '100%', height: '100vh'
     }}>
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>
+        logout
+      </button>
     </div>
   )
 }
